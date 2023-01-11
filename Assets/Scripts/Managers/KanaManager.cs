5dca398968ad45a93b7557f079bfbae04adc7f1a
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-01-11 10:34:04 +0000

    Add spawning system
    Checks for free spawns and stops spawning when all spawns are full

--- Assets/Scripts/Managers/KanaManager.cs
+++ Assets/Scripts/Managers/KanaManager.cs
@@ -12,14 +12,42 @@ public class KanaManager : MonoBehaviour
     [SerializeField]
     Spawn[] spawns;
 
-    Spawn[] freeSpawns;
-    Spawn[] occupiedSpawns;
+    [SerializeField]
+    List<Spawn> freeSpawns = new List<Spawn>();
+    [SerializeField]
+    List<Spawn> occupiedSpawns = new List<Spawn>();
+
+
+
+
+
+    private void Awake()
+    {
+
+        //Fill free spawns
+        foreach(Spawn spawn in spawns)
+        {
+            freeSpawns.Add(spawn);
+        }
+
+
+
+
+
+    }
+
 
 
     Spawn getRandomFreeSpawn()
     {
-        int index = Random.Range(0, freeSpawns.Length);
-        Spawn freeSpawn = spawns[index];
+        if(freeSpawns.Count == 0)
+            return null;
+
+
+        int index = Random.Range(0, freeSpawns.Count);
+        Spawn freeSpawn = freeSpawns[index];
+        occupiedSpawns.Add(freeSpawn);
+        freeSpawns.Remove(freeSpawn);
         //Move free spawn to occupied spawn and then spawn kana dont do this function withot spawning kana or then it will be stuck in limbop please dont do this i beg you
         
 
@@ -32,8 +60,16 @@ public class KanaManager : MonoBehaviour
 
     void SpawnNewKana()
     {
+        //Get random free spawn
+        Spawn spawn = getRandomFreeSpawn();
+        if(!spawn)
+        {
+            //Deal damage as their isn't any more kana spots
+            return;
+        }
+        //Get new kana
         Kana k = factory.Get();
-        k.transform.position = spawns[Random.Range(0, spawns.Length)].spawnPoint.position;
+        k.transform.position = spawn.spawnPoint.position;
         kanaList.Add(k);
     }
 
